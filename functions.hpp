acd572b670335828824e56d98ae82f049cb62931
codymorse5981 <40225014+codymorse5981@users.noreply.github.com>
2018-06-14 02:21:45 -0500

    Create functions.hpp

---
+++ functions.hpp
@@ -0,0 +1,21 @@
+#ifndef _FUNCTIONS_HPP
+#define _FUNCTIONS_HPP
+
+#include <iostream>
+using namespace std;
+
+int Choice(int min, int max);
+
+string nameOfPet();
+
+void displayCat();
+
+void displayBunny();
+
+void displayFish();
+
+void displayPetMenu();
+
+int petChoice(int min, int max);
+
+#endif
