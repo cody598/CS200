6082a3e61f835e6527e53ff5ad5f0e7fbdb1a9e9
codymorse5981 <40225014+codymorse5981@users.noreply.github.com>
2018-06-13 20:01:32 -0500

    Create functions.hpp

---
+++ Part1/functions.hpp
@@ -0,0 +1,12 @@
+#ifndef _FUNCTIONS_HPP
+#define _FUNCTIONS_HPP
+
+#include <iostream>
+using namespace std;
+
+
+void DisplayMainMenu();
+int GetChoice(int min, int max);
+string GetToDoItem();
+
+#endif
