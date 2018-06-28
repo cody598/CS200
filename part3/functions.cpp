835ca1a24c0b79ac1ea7422d0af541c47ec944f7
codymorse5981 <40225014+codymorse5981@users.noreply.github.com>
2018-06-27 19:27:49 -0500

    Create functions.cpp

---
+++ part3/functions.cpp
@@ -0,0 +1,12 @@
+#include "functions.hpp"
+#include <iostream>
+#include <string>
+#include <cstdlib>      // to use rand()
+#include <ctime> 
+
+int getSize()
+{
+	int size;
+	cin >> size;
+	return size;
+}
