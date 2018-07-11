d1a457a03d162b40db5707b79c3ecc59cc32d208
codymorse5981 <40225014+codymorse5981@users.noreply.github.com>
2018-07-11 18:47:15 -0500

    Create AdventureGame.cpp

---
+++ AdventureGame/AdventureGame.cpp
@@ -0,0 +1,21 @@
+#include "AdventureGame.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void AdventureGame::Setup()
+{
+}
+
+void AdventureGame::Run()
+{
+}
+
+void AdventureGame::Move(const string& direction)
+{
+}
+
+char AdventureGame::GetInput()
+{
+}
